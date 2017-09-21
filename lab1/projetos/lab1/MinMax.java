283bd8234ae60f13d93d956806b57dc94de134a3
Usuário <Usurio@DESKTOP-8LIEST6>
2017-09-21 19:09:47 -0300

    Organizando pastas

--- lab1/projetos/lab1/MinMax.java
+++ lab1/projetos/lab1/MinMax.java
@@ -8,16 +8,16 @@ public class MinMax {
 	      final int NÚMEROS_A_LER = 3;
 	      Scanner sc = new Scanner(System.in);
 	      int min = Integer.MAX_VALUE;
-	      int máx = Integer.MIN_VALUE;
+	      int max = Integer.MIN_VALUE;
 	 
 	      for (int i = 0; i < NÚMEROS_A_LER; i++) {
 	        int num = recebeProximoInteiro(sc);
 	        min = menorNumeroEntre(min, num);
-	        máx = maiorNumeroEntre(máx, num);
+	        max = maiorNumeroEntre(max, num);
 	      }
 	 
 	      System.out.println("O menor numero é: " + min);
-	      System.out.println("O maior numero é: " + máx);
+	      System.out.println("O maior numero é: " + max);
 	   }
 	 
 	   private static int maiorNumeroEntre(int numero1, int numero2) {
