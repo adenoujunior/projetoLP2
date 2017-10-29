23a989ccd03bbb0c0fe695218129b435729ca6dd
Adenou Dantas de Farias Júnior <adenoujunior@gmail.com>
2017-10-29 12:50:50 -0300

    LabProg05

--- Lab05/src/projeto/TestaRobo.java
+++ Lab05/src/projeto/TestaRobo.java
@@ -1,13 +1,12 @@
 /*Aluno 1: Adenou Dantas De Farias Júnior*/
 /*Aluno 2: <seu nome>*/
 /*Aluno 3: <seu nome>*/
-
 package projeto;
 
 /**
  *
  * @author Adenou
  */
-public class Sala {
+public class TestaRobo {
     
 }
